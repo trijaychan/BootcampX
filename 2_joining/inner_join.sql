bcc141edbe2c6936cf8533e7efde92731be96673
trijaychan <trijaychan@gmail.com>
2022-03-07 19:08:56 +0000

    updated filename and finished JOIN exercises

--- 2_joining/inner_join.sql
+++
@@ -1,2 +0,0 @@
-SELECT students.name as student_name, email, cohorts.name as cohort_name
-FROM students JOIN cohorts ON cohort_id = cohorts.id;
--- 2_joining/outer_join.sql
+++
@@ -1,2 +0,0 @@
-SELECT students.name as student_name, email, cohorts.name as cohort_name
-FROM students OUTER JOIN cohorts ON cohorts.id = cohort_id;
